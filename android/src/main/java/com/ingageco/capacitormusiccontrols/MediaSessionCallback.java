ed426a7d60726b5d4ebf3da687c16e63d40d9280
Leo Chang-Liao <35870762+leochangliao@users.noreply.github.com>
2025-01-01 12:47:48 -0500

    Update MediaSessionCallback.java
    Add onSeekTo function

--- android/src/main/java/com/ingageco/capacitormusiccontrols/MediaSessionCallback.java
+++ android/src/main/java/com/ingageco/capacitormusiccontrols/MediaSessionCallback.java
@@ -18,6 +18,17 @@ public class MediaSessionCallback extends MediaSessionCompat.Callback {
   }
 
   @Override
+  public void onSeekTo(long pos) {
+    super.onSeekTo(pos);
+
+    JSObject ret = new JSObject();
+    ret.put("message", "music-controls-seek-to");
+    ret.put("position",pos);
+    this.musicControls.controlsNotification(ret);
+
+  }
+  
+  @Override
   public void onPlay() {
     super.onPlay();
 
@@ -135,4 +146,3 @@ public class MediaSessionCallback extends MediaSessionCompat.Callback {
     return true;
   }
 }
-
